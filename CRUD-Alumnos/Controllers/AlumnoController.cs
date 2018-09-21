ad75632d8fd3d6ee77d43756f8995a91c5987c9c
Jhon Alzate <jalzate0128@gmail.com>
2018-09-20 22:59:44 -0500

    Vistas lista y creación
    Se arma arquitectura del proyecto y se generan vista de lista y creación de alumnos

---
+++ CRUD-Alumnos/Controllers/AlumnoController.cs
@@ -0,0 +1,33 @@
+﻿using CRUD_Alumnos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_Alumnos.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            AlumnosContext db = new AlumnosContext();
+
+            //List<Alumno> lista = db.Alumno.Where(a => a.Edad > 18).ToList();
+            return View(db.Alumno.ToList());
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(Alumno a)
+        {
+            return View();
+        }
+    }
+    
+}
